aaabe16da90bb03a32a1956618654c58ae2a2274
Luckyluc96 <Lucsaybibi@gmail.com>
2025-07-23 20:16:20 -0400

    Applies a haste on map change.. will acquire the map IDs so this only effects relevant maps such as sanctuary cities. Will add logic to gatekeep buff to relevant cities only so "enemy" players cannot be buffed in an enemy town.

--- faster_crafting.lua
+++ faster_crafting.lua
@@ -1,12 +1,16 @@
-function OnMapChange(event, player)
-    haste_aura = 32693
-    isRested = player:IsRested()
-    if isRested then
+function OnMapChange(event, player, map)
+    haste_aura = 63150
+    mapName = player:GetMap():GetName()
+    allianceMaps = {}
+    hordeMaps = {}
+    isAlliance = player:IsAlliance()
+
+    if mapName and isAlliance then
+        player:SendBroadcastMessage(string.format("The map you just joined is: %s", mapName))
         player:AddAura(haste_aura, player)
     else
         player:RemoveAura(haste_aura)
     end
-    
 end
 
 RegisterPlayerEvent(28, OnMapChange)
