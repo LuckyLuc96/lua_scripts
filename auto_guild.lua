ec5098ae61e6dfc78142b88b1541062df2925dd4
Luckyluc96 <Lucsaybibi@gmail.com>
2025-08-15 14:53:33 -0400

    Added autoguild script testing faster_mount script on trinitycore. Needs work

---
+++ auto_guild.lua
@@ -0,0 +1,12 @@
+GuildName = "test"
+
+local function ON_FIRST_LOGIN(event, player)
+    local guild = GetGuildByName(GuildName)
+    if guild then
+        guild:AddMember(player, 0)
+    else
+        print("Guild not found!")
+    end
+end
+
+RegisterPlayerEvent(30, ON_FIRST_LOGIN)
