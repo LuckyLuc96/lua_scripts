6f2e06df7c924685ddc9da6b916adc85d3ec0f94
Luckyluc96 <Lucsaybibi@gmail.com>
2025-08-16 16:35:30 -0400

    fix scheduler logic

--- BlackMarket/BlackMarket - Scheduler.sql
+++ BlackMarket/BlackMarket - Scheduler.sql
@@ -1,15 +1,10 @@
 SET GLOBAL event_scheduler = ON;
-DROP EVENT IF EXISTS UpdateBlackMarketEvent$$
+DROP EVENT IF EXISTS UpdateBlackMarketEvent;
 
 -- Will create an sql event that on a weekly basis refreshs the weekly black market item sets
 -- You can run this .sql script to delete and remake the event so you can set the start date when you like
-DELIMITER $$
 CREATE EVENT UpdateBlackMarketEvent
 ON SCHEDULE EVERY 1 WEEK
 STARTS CURRENT_DATE + INTERVAL (7 - WEEKDAY(CURRENT_DATE)) DAY + INTERVAL 0 HOUR
 COMMENT 'Automatically refreshes Black Market item sets weekly'
-DO
-BEGIN
-    CALL UpdateBlackMarketSets();
-END$$
-DELIMITER ;
+DO CALL UpdateBlackMarketSets();
